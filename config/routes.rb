3efe3d0d3ff0c069d7c7fc4fcc563b9f766c76fa
simmani91 <devsimmani@gmail.com>
2014-08-04 20:28:00 +0900

    make model notification and associate with user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  resources :users do
+    resources :notifications
+  end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
