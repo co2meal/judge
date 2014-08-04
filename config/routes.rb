d3e3b0ce1bf91e2e20061b85d4f07d9139d26fdb
algedian <algedian8008@gmail.com>
2014-08-04 20:28:01 +0900

    welcome page 만듦
    ㅇㅇ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
-Rails.application.routes.draw do
+Judge::Application.routes.draw do
+  
+  root 'welcome#index'
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
