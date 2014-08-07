9b1f0b6854e402e9c8baea04dc180273b3d0c1dd
nicola <co2meal@gmail.com>
2014-08-07 12:38:11 +0000

    Seed A + B problem in fixtures

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140804164141) do
+ActiveRecord::Schema.define(version: 20140806114824) do
+
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
     t.text     "body"
@@ -44,9 +45,6 @@ ActiveRecord::Schema.define(version: 20140804164141) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
-
-  add_index "models", ["email"], name: "index_models_on_email", unique: true
-  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
 
   create_table "notifications", force: true do |t|
     t.string   "subject"
