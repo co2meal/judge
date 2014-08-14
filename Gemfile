d547a51bd352eeaf7e1e6a4cb4f1e7c4f505f420
simmani91 <devsimmani@gmail.com>
2014-08-14 13:44:08 +0900

    how to truble shoot
    망케라 ㅈ�

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 gem 'activeadmin', github: 'gregbell/active_admin'
 gem 'redcarpet'
+gem "paperclip", "~> 4.1"
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140807152150) do
+ActiveRecord::Schema.define(version: 20140814034640) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -107,6 +107,10 @@ ActiveRecord::Schema.define(version: 20140807152150) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
