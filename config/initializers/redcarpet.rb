dd14ac8431fa943418db12ec364b9d4333feac7b
nicola <co2meal@gmail.com>
2014-08-25 06:33:15 +0000

    Improve Problem View

--- config/initializers/redcarpet.rb
+++ config/initializers/redcarpet.rb
@@ -1,5 +1,8 @@
-class RenderHTMLWithTable < Redcarpet::Render::HTML
+class RenderProblem < Redcarpet::Render::HTML
   def table(header, body)
     '<table class="table">' + header + body + '</table>'
+  end
+  def codespan(code)
+    '<kbd>' + code + '</kbd>'
   end
 end
